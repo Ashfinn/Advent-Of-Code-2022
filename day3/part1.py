02365af16aa0fea1e5a747d3bfa0c552183ddb1e
Ashfinn <obidur.shawal@gmail.com>
2022-12-07 09:18:11 +0600

    modified:   day3/part1.py

--- day3/part1.py
+++ day3/part1.py
@@ -1,12 +1,26 @@
-comp = open("day3/input.txt", "r")
+comp = open("input.txt", "r")
 c = comp.read()
 li = c.split("\n")
+points=0
 
-for i in li:
-    if i == "":
-        li.remove(i)
-    
 
-print(li)
+def point_table():
+    p = 0
+    while p < 26:
+
+
+
+for i in range(len(li)):
+    first_half = li[:len(li)//2]
+    second_half = li[len(li)//2:]
+    n=0
+    while n < len(first_half):
+        if first_half[n] ==second_half[n]:
+            if first_half[n] == first_half[n].upper:
+                points += 
+
+print(first_half)
+print(second_half)
+
 
 comp.close()
